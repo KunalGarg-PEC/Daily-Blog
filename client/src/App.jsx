7e73b4e10761d15b86a7c6bb8431f432fa526cb1
KunalGarg-PEC <gargkunal369@gmail.com>
2024-07-12 15:00:20 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Search from './pages/Search';
 import SignIn from './pages/SignIn';
 
+
 export default function App() {
   return (
     <BrowserRouter>
