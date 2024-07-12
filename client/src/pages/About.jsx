b7ef07b480f42bebd196239c829bf622a417ab69
KunalGarg-PEC <gargkunal369@gmail.com>
2024-07-12 13:12:28 +0530

    mern blog

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Daily Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Daily is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
